dc21f6526ff57b6c80ed306a633a980536ea814b
BrianHGrant <bhgrant8@gmail.com>
2016-07-15 08:50:20 -0700

    add passing specs for returning other attributes for instance of Stylist class (:last_name and :id) ;

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -7,4 +7,17 @@ describe ('Stylist') do
       expect(test_stylist.first_name()).to(eq("Veronica"))
     end
   end
+
+  describe('#last_name') do
+    it('will return the last name of a stylist') do
+      test_stylist = Stylist.new(:first_name => "August", :last_name => "West")
+      expect(test_stylist.last_name()).to(eq("West"))
+    end
+  end
+
+  describe('#id') do
+    it('will return the id of a stylist') do
+      test_stylist = Stylist.new(:id => 75, :first_name => "August", :last_name => "West")
+    end
+  end
 end
